791803e402d7768cdc51a730aac7e8178ca30b5e
antoine-humbert <antoine@skale-5.com>
2017-07-11 21:35:21 +0200

    update template chateau

--- kiliman/templates/chateau/config.js
+++ kiliman/templates/chateau/config.js
@@ -1,9 +1,9 @@
 // RethinkDB settings
-exports.host = '163.172.182.154';    // RethinkDB host
+exports.host = '192.168.0.11';    // RethinkDB host
 exports.port = 28015;
-exports.user = '';
+exports.user = 'admin';
 exports.password = '';
-exports.authKey = '';          // Authentification key (leave an empty string if you did not set one)
+//exports.authKey = '';          // Authentification key (leave an empty string if you did not set one)
 
 // Express settings
 exports.expressPort = 3000;    // Port used by express
