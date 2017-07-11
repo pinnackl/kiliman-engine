fac5d7aa3079c9354b363c34f0e8c5f0752fadad
antoine-humbert <antoine@skale-5.com>
2017-07-11 15:09:50 +0200

    build for prod

---
+++ docker/chateau/chateau_docker/config.js
@@ -0,0 +1,9 @@
+// RethinkDB settings
+exports.host = '192.168.1.24';    // RethinkDB host
+exports.port = 29015;          // RethinkDB driver port
+exports.authKey = '';          // Authentification key (leave an empty string if you did not set one)
+
+// Express settings
+exports.expressPort = 3000;    // Port used by express
+exports.debug = true;          // Debug mode
+exports.network = '0.0.0.0'  // Network the node app will run on
---
+++ docker/srv/horizon/chateau/config.js
@@ -0,0 +1,10 @@
+// RethinkDB settings
+exports.host = '163.172.182.154';    // RethinkDB host
+exports.port = 28015;
+exports.user = 'admin';     // RethinkDB driver port
+//exports.authKey = '';          // Authentification key (leave an empty string if you did not set one)
+
+// Express settings
+exports.expressPort = 3000;    // Port used by express
+exports.debug = true;          // Debug mode
+exports.network = '0.0.0.0'  // Network the node app will run on
